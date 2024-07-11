cfa534f7250eca58d80396d8739749d8e6ede9fe
Antoine BERNIER <antoine.bernier@gmail.com>
2024-07-11 11:52:40 +0200

    overrides

--- components/Typography.tsx
+++
@@ -1,7 +0,0 @@
-export function H1({ children }: { children?: React.ReactNode }) {
-  return (
-    <h1 className="text-4xl uppercase leading-none tracking-tight sm:text-5xl md:text-7xl">
-      {children}
-    </h1>
-  )
-}
